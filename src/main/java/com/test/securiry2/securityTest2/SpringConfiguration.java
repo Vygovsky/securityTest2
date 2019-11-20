06b984ac5d21b4ed4fab74371029e314d43d150c
Roman_v <vigmol@ukr.net>
2019-11-20 23:14:20 +0200

    add withDefaultSchema

--- src/main/java/com/test/securiry2/securityTest2/SpringConfiguration.java
+++ src/main/java/com/test/securiry2/securityTest2/SpringConfiguration.java
@@ -1,25 +1,43 @@
 package com.test.securiry2.securityTest2;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.sql.DataSource;
+
 @EnableWebSecurity
 public class SpringConfiguration extends WebSecurityConfigurerAdapter {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("anon")
-                .password("anon")
-                .roles("USER")
-                .and()
-                .withUser("roman")
-                .password("roman")
-                .roles("ADMIN");
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .withDefaultSchema()
+                .withUser(
+                        User.withUsername("pol")
+                                .password("pol")
+                                .roles("USER")
+                )
+                .withUser(
+                        User.withUsername("roman")
+                                .password("pass")
+                                .roles("ADMIN")
+                );
     }
 
     @Bean
@@ -35,5 +53,6 @@ public class SpringConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/").permitAll()
                 .and().formLogin();
     }
+
 }
 
